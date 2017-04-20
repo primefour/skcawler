6c82762d22b5a1cb92a3994914b73c22ab8b6fd1
crazyhorse <primefour@163.com>
2017-04-20 21:57:19 +0800

    downloader is all right

---
+++ downloader/downloader.go
@@ -0,0 +1,43 @@
+package downloader
+
+import "fmt"
+import "io"
+import "bufio"
+import "bytes"
+
+func Downloader(url string) ([]byte, error) {
+	http_req := NewHttpRequest()
+	http_req.Url = "http://www.sina.com.cn"
+	http_req.Method = HTTP_GET
+	task, err := NewHttpTask(*http_req)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := task.DoTask()
+	if err != nil {
+		fmt.Printf("get a error %v ", err)
+		return nil, err
+	}
+
+	var buff bytes.Buffer
+
+	reader := bufio.NewReader(resp.Body)
+	defer resp.Body.Close()
+	var buffer = make([]byte, 1024)
+	for true {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Printf("get a eof")
+			} else {
+				return nil, err
+			}
+			break
+		}
+		if n > 0 {
+			buff.Write(buffer)
+		}
+	}
+	fmt.Printf("buffer %s \n", buff)
+	return buff.Bytes(), nil
+}
--- main.go
+++ main.go
@@ -1,33 +1,11 @@
 package main
 
 import "fmt"
-import "io"
 import "github.com/primefour/skclawer/downloader"
 
 func main() {
-	http_req := downloader.NewHttpRequest()
-	http_req.Url = "http://www.baidu.com"
-	http_req.Method = downloader.HTTP_GET
-	task, err := downloader.NewHttpTask(*http_req)
+	_, err := downloader.Downloader("http://www.baidu.com")
 	if err != nil {
-		return
-	}
-	resp, err := task.DoTask()
-	if err != nil {
-		fmt.Printf("get a error %v ", err)
-	}
-	reader := resp.Body
-	defer reader.Close()
-	var buffer [10]byte
-
-	for true {
-		n, err := reader.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Printf("get a eof")
-			}
-			break
-		}
-		fmt.Printf("buffer %s ", buffer)
+		fmt.Printf("%v ", err)
 	}
 }
